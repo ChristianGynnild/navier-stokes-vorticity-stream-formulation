f7cc0d9d9e0f88ed3ec64c8ae0f56b98f9395ea6
christiangynnild <75206913+ChristianGynnild@users.noreply.github.com>
2025-12-27 05:36:56 +0100

    Simple monocolor background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use faer::{Mat, Scale, mat};
+mod rendering;
+
+
 fn main() {
+
+
+
+
+
+
+
+    let X_LENGTH = 128;
+    let Y_LENGTH = 128;
+
+    let vorticity = Mat::from_fn(X_LENGTH, Y_LENGTH, |i, j| (i + j) as f64);
+    
+
+    rendering::run();
+    
     println!("Hello, world!");
+
+
 }
+
+
+
